c24a978e32f7d3a1d916369b61c8910f15aed5b1
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-29 16:54:04 -0400

    Creating basic tic tac toe board and basic game logic

---
+++ apps.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+var turn = 1;
+
+
+
+
+$('td').on('click', function() {
+  if (turn % 2 === 0) {
+     $(this).text('x').addClass('x');
+   } else {
+     $(this).text('o').addClass('o');
+   }
+   $(this).off('click');
+   turn++;
+   determineWinnerX();
+   determineWinnerO();
+});
+
+
+
+function determineWinnerX() {
+  if (($('#one').hasClass('x') && $('#two').hasClass('x') && $('#three').hasClass('x'))
+  ||
+  ($('#four').hasClass('x') && $('#five').hasClass('x') && $('#six').hasClass('x'))
+  ||
+  ($('#seven').hasClass('x') && $('#eight').hasClass('x') && $('#nine').hasClass('x'))
+  ||
+  ($('#one').hasClass('x') && $('#four').hasClass('x') && $('#seven').hasClass('x'))
+  ||
+  ($('#two').hasClass('x') && $('#five').hasClass('x') && $('#eight').hasClass('x'))
+  ||
+  ($('#three').hasClass('x') && $('#six').hasClass('x') && $('#nine').hasClass('x'))
+  ||
+  ($('#one').hasClass('x') && $('#five').hasClass('x') && $('#nine').hasClass('x'))
+  ||
+  ($('#three').hasClass('x') && $('#five').hasClass('x') && $('#seven').hasClass('x')))
+  {
+    var winner = 'x';
+  }
+}
+
+function determineWinnerO() {
+  if (($('#one').hasClass('o') && $('#two').hasClass('o') && $('#three').hasClass('o'))
+  ||
+  ($('#four').hasClass('o') && $('#five').hasClass('o') && $('#six').hasClass('o'))
+  ||
+  ($('#seven').hasClass('o') && $('#eight').hasClass('o') && $('#nine').hasClass('o'))
+  ||
+  ($('#one').hasClass('o') && $('#four').hasClass('o') && $('#seven').hasClass('o'))
+  ||
+  ($('#two').hasClass('o') && $('#five').hasClass('o') && $('#eight').hasClass('o'))
+  ||
+  ($('#three').hasClass('o') && $('#sio').hasClass('o') && $('#nine').hasClass('o'))
+  ||
+  ($('#one').hasClass('o') && $('#five').hasClass('o') && $('#nine').hasClass('o'))
+  ||
+  ($('#three').hasClass('o') && $('#five').hasClass('o') && $('#seven').hasClass('o')))
+  {
+    var winner = 'x';
+  }
+}
+
+
+});
